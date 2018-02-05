93d3ea2db0681981e870f6af111893f436d6dba3
lucas <lucasborio@gmail.com>
2018-02-05 10:22:35 -0300

    menu

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -2,18 +2,15 @@
 
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
    
-      <a class="navbar-brand" href="#">WeatherApp</a>
+      <a class="navbar-brand" href="/">WeatherApp</a>
          <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <div class="collapse navbar-collapse" id="navbarsDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Forecast</a>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
         </ul>
         
